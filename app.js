18701ffc0b0f4ecad78cf36d36c3e7450af05b1b
Mohammed saud <mdsaud2200@gmail.comgit config --global user.name Mohammed>
2024-09-07 16:37:00 +0530

    first commmit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req,res)=>{
+  res.render("index.ejs")
+});
+
+
+const Port = 3030;
+app.listen(Port,()=>{
+  console.log('Run..');
+})
